22b15de35d549db7a2838e1c90c451c4e68bedb7
Manafredo <67021716+Manafredo@users.noreply.github.com>
2021-02-09 20:04:17 +0100

    Update Person View

--- App.svelte
+++ App.svelte
@@ -1,17 +1,23 @@
 <script>
-  import Button from "./Button.svelte";
+  import FilterCategories from "./FilterCategories.svelte";
+  import Persons from "./Persons.svelte";
+
   import { onMount } from "svelte";
+
+  let persons = [];
+  let filter;
+
+  onMount(async function() {
+    const response = await fetch(
+      "https://cdn.contentful.com/spaces/t170cpyn3oju/environments/master/entries/?content_type=person&include=0&select=sys.id,fields&access_token=MFnR8m8akJLpWiIGbewXZi_PgdWJ0lWv46tjhf7g4uU"
+    );
+    persons = await response.json();
+    persons = persons.items;
+  });
 </script>
 
 <style>
-  main {
-    font-family: sans-serif;
-    text-align: center;
-  }
 </style>
 
-<main>
-	<h1>Hello CodeSandbox</h1>
-	<h2>Start editing to see some magic happen!</h2>
-	<Button />
-</main>
+
+<Persons {persons}/>
---
+++ FilterCategories.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let filter = "all";
+  export let onclick = clicked => {};
+  $: onclick(filter);
+</script>
+
+<div class="filterCategories btn-group stack-exception">
+  <button class="btn toggle-btn" class:btn__primary={filter === 'Politiker'} aria-pressed={filter === 'all'} on:click={()=> filter = 'Politiker'} >
+    <span>Politiker</span>
+  </button>
+  <button class="btn toggle-btn" class:btn__primary={filter === 'Influencer'} aria-pressed={filter === 'active'} on:click={()=> filter = 'Influencer'} >
+    <span>Influencer</span>
+  </button>
+  <button class="btn toggle-btn" class:btn__primary={filter === 'Sonstiges'} aria-pressed={filter === 'completed'} on:click={()=> filter = 'Sonstiges'} >
+    <span>Sonstiges</span>
+  </button>
+</div>
